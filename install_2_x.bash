684751a6f58738332db3cc110aa3f64d08aac332
Neil Harrold <neil@localhost.localdomain>
2017-02-01 22:45:36 +0000

    initial commit

---
+++ install_2_x.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum remove -y elasticsearch
+sudo rm -rf /var/lib/elasticsearch
+sudo cp elasticsearch_2_x.repo /etc/yum.repos.d/elasticsearch.repo
+sudo yum install -y elasticsearch
+sudo cp elasticsearch_2_x.yml /etc/elasticsearch/elasticsearch.yml
+sudo chkconfig elasticsearch on
+sudo service elasticsearch start
+sudo yum install elasticsearch
+sudo /usr/share/elasticsearch/bin/plugin install mobz/elasticsearch-head
